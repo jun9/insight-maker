6edb1ca878028284ea47a8b3d1845847c180632e
scishop <scishop@840a6397-965c-49d0-a6b3-48a640647949>
2010-04-30 17:07:27 +0000

    test commit
    git-svn-id: https://insightmaker.svn.sourceforge.net/svnroot/insightmaker@2 840a6397-965c-49d0-a6b3-48a640647949

--- designer.php
+++ designer.php
@@ -5,7 +5,6 @@ require("SharedHeader.php")
 <div id="mpanel">
 	</div>
 	<script type="text/javascript">
-	
 		var storeData = <?php 
 		if ($node->field_ui[0]['value'] != ""){
 		 print(str_replace("script","scriptscript",$node->field_ui[0]['value']));
